307d76536caee43811ee703f093ce418e0cb9702
Jeon Soo Jeong <111031449+asel0joo@users.noreply.github.com>
2023-03-06 11:51:59 +0900

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String s) {
+
+        StringBuilder sb = new StringBuilder(s);
+        String answer = sb.reverse().toString();
+
+        return answer;
+    }
+}
